5e3209fc22d7e507fa3d1285a06b634b25426edd
hitesh1422 <hitesh.m1422@gmail.com>
2024-04-18 20:59:54 +0530

    update

---
+++ Day-2/index.js
@@ -0,0 +1,49 @@
+var myfirstmarks = 90
+var mysecondmarks = 86
+
+console.log(myfirstmarks + mysecondmarks);
+console.log(myfirstmarks - mysecondmarks);
+console.log(myfirstmarks * mysecondmarks);
+console.log(myfirstmarks / mysecondmarks);
+
+console.log(10,5,typeof(10))
+console.log("Hi",typeof("Hi"))
+console.log('Hi',typeof('Hi'))
+console.log(`Hi`,typeof('Hi'))
+console.log(true,typeof(true))
+console.log(false,typeof(false))
+console.log('433',typeof('433'))
+
+
+var myname = "Hitesh"
+console.log(myname,typeof(myname))
+
+var todaysstudent
+console.log(todaysstudent,typeof(todaysstudent))
+
+
+console.log(myclass)
+var myclass = 40 //declare
+console.log(myclass)
+myclass = 30 //reassign
+console.log(myclass)
+var myclass = 50 // re-declare
+console.log(myclass)
+
+let product = "cloth" //declare
+console.log(product)
+
+product = "shooes" // re-assign
+console.log(product)
+
+// let product = "cap" // re declare
+// console.log(product)
+
+const mynumber = 22 // declare
+console.log(mynumber)
+
+//mynumber = 76 // re assign
+//console.log(mynumber)
+
+//const mynumber = 90 // re-declare
+//console.log(mynumber)
