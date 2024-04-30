75a11e5d8d096a66301ac54151ce49f1bf92056a
hitesh1422 <hitesh.m1422@gmail.com>
2024-04-30 14:59:34 +0530

    update

---
+++ Day-7/index.js
@@ -0,0 +1,22 @@
+// for(starting, ending,sequence){
+// code
+// }
+
+
+
+// Q. find odd number in  addition number from array
+
+
+// Q. print number from 10 to 60 with gap of 4
+
+for (var i = 10; i <= 60; i += 4){
+console.log(i);
+}
+
+
+// Q. print number from 33 to 76 wit gap of 3 but number also must be even
+
+//for(var x = 33; x <= 60; x += 3){
+  //  console.log(x);
+//}
+
