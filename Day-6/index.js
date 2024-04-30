aabc6fac1e006a54cbdaef2cd8c6d8cc6ddec594
hitesh1422 <hitesh.m1422@gmail.com>
2024-04-30 11:54:54 +0530

    update

--- Day-6/index.js
+++ Day-6/index.js
@@ -1,11 +1,42 @@
-function checkage(){ 
+//function checkage(){ 
+    //  alert("Hi")
+  //}
+  //let age = 18;
+  //if(age <= 17){
+    // alert("You are a teenager and cannot vote");
+  //} else{
+    //alert('Your are not eligiable for vote');
+  //}
+//}
+  
 
-const age = (documnet.getElementById("age").value);
-if(!age){
-    alert(Please enter age);
-} else if (age > 18){
-    alert("Eligible for driving licence");
-} else (age < 18){
-    alert ("your not aligible for driving licence");
+
+var std1marks = 40;
+var std2marks = 60;
+var std3marks = 80;
+console.log(std1marks>std2marks);
+console.log(std1marks>std3marks);
+console.log(std2marks>std3marks);
+console.log(40>60 && 40>80 && 60>80);
+console.log(40>60 || 40>80 || 60>80);
+
+
+
+// switch case
+
+var studentname = "Hitesh"
+switch (studentname){
+        case "Ramesh":
+        console.log("The student name is Ramesh");
+        break
+        case "Santosh":
+        console.log("The student name is Santosh");
+        break
+        case "Priya":
+        console.log("The student name is Priya");
+        break
+        case "Hitesh":
+        console.log("The student name is Hitesh");
 }
-}
+
+
