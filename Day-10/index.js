57247fd0481211543c10d7559b3ef19279f70069
hitesh1422 <hitesh.m1422@gmail.com>
2024-05-06 11:00:23 +0530

    updae

---
+++ Day-10/index.js
@@ -0,0 +1,22 @@
+// push
+var stdname = ['Jayesh','Prachi','Prisha'];
+stdname.push('Hitesh');
+console.log(stdname);
+
+// pop
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+console.log(fruits);
+
+
+// splicec
+const animals = ["Dog","Cat","Cow","Parot"];
+animals.splice(2,0,"Crow","Buterfly");
+console.log(animals);
+
+
+//sort
+const number = [81,20,50,34,5];
+number.sort();
+console.log(number);
+
